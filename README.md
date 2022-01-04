4c4baf6b611594dee27667d25d77b56189ac64c0
JamCow <jamcow+github@gmail.com>
2022-01-04 07:52:44 +0000

    JS refactor, added avif and heif

--- README.md
+++ README.md
@@ -2,22 +2,36 @@
 
 NodeJS batch image resize and format using Sharp and libvips
 
-## Setup
+## Setup and run
 
-1. `yarn`
-2. `node index.js`
+1. Put image(s) in the `img/in/` folder
+2. `yarn`
+3. `node index.js`
 
 ## What is this
 
-This outputs all the images in the `img/in/` folder, includes them in a bunch of badly coded Javascript Promises, convoluted forEach loops, and then spits them out in multiple sizes, quality settings, and formats. It also makes some cropped images with those settings too.
+JPG in, optimised JPG and WEBP out.
 
-It works for my needs :)
+Originally for outputting image galleries (multi-thousand). For X number of photos, Photoshop took 40 seconds, libvips was around 8 seconds. Resizing, applying a watermark and outputting thumbnail and image.
+
+This processes and outputs all the images in the `img/in/` folder, includes them in a bunch of badly coded Javascript Promises, convoluted forEach loops, and then spits them out in multiple sizes, quality settings, and formats. It also makes some cropped images with those settings too.
+
+Can be better optimised to use CPU cores better, group formats together, use buffer/pipelines optimally, etc... And config by default outputs lots of variations. This was also made for generating placeholder stock images to go on a cheap CDN.
+
+You could run a server to dynamically generate images with dynamic sizes, but issues are: cost (if you have the money - just use cloudinary/imgix), server CPU, and caching/security). I didn't want to bother with all that, and just stuck the static images on a CDN (private and secure).
 
 ### Input
 
 `file.jpg`
 
 ### Output
+
+Each input image will generate:
+
+- original ratio, and 4 custom ratios (4:6, 16:9, 22:9, 1:1)
+- each custom ratio has 3 crop positions (centre, attention, entropy)
+- there are 10 image widths for each original (1 &times; 10), and custom ratio with crop position ((4 &times; 3) &times; 10) (100px, 150px, 200px, 400px, 600px, 800px, 1000px, 1200px, 1600px, 2000px)
+- total of 130 files &times; each image format &times; original file.
 
 `{originalFilename}-{size}@{cropRatio}.jpg`
 
@@ -64,3 +78,9 @@ size = 01 (smallest @ 100px) to 10 (largest @ 2000px)
 - input-10@1_1.jpg
 - input-10@22_9.jpg
 - [and all the above, in `*.webp`]
+
+## AVIF
+
+In testing, quality values of `6, 7, 8, and 9` generated identical file output (SHA512).
+Even with `quality: 60`, `speed 0`, filesizes aren't much lower (sometimes larger if quality is above 60) than a well optimised Mozjpeg JPG.
+Definitely going to be used in the future, but needs a fair amount of optimisation, and especially careful to decide if applying to picture content. Detailled images can remain crisp, but images with mid-tones/hazy (eg, clouds, misty fields) tend to suffer blurring and loss of details, especially if the image has detailled areas too. Similar to webp in that regard.
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // const fsPromises = fs.promises;
 // https://stackoverflow.com/questions/40593875/using-filesystem-in-node-js-with-async-await#answer-47190904
 
@@ -16,229 +14,338 @@
 // https://github.com/lovell/sharp/issues/1251#issuecomment-395011222
 // https://github.com/lovell/sharp/issues/1251#issuecomment-447209921
 
-// function isArrayEmpty(array){
-//     return !Array.isArray(array) || !array.length
-// }
+// const fs = require('fs');
+const mkdirp = require('mkdirp');
+const sharp = require('sharp');
+const path = require('path');
+const glob = require('glob');
 
-const mkdirp = require('mkdirp')
-// mkdirp https://github.com/lovell/sharp/issues/1052
+const config = {
+    input: './img/in/',
+    output: './img/out/',
+    // Beware if same basename, it will overwrite to leave only one (no filename collision detection)
+    globPattern: `*.jpg`, // `*.jpg,*.jpeg,*.png`,
 
-const sharp = require("sharp");
-const fs = require("fs");
-const path = require("path");
-const glob = require("glob")
+    makeRectangles: true,
+    makeRatioCrops: true,
+    makeJpeg: true,
+    makeWebp: true,
+    makeAvif: false,
+    makeHeif: false,
+    positionStrategies: [
+        // [filenamePrefix, Strategy] (benchmark: 5 images, 4 ratios, 10 sizes = 500 images)
+        ['-c', 'centre'], // 'centre' very fast (1.00)
+        ['', 'attention'], // 'attention' is visually better in majority (1.75)
+        ['-e', 'entropy'], // 'entropy' looks for busy/complexity, visually worse in majority (2.25)
+    ],
+    imageRatios: [
+        [4, 6],
+        [16, 9],
+        [22, 9],
+        [1, 1],
+    ],
+    imageSizes: [100, 150, 200, 400, 600, 800, 1000, 1200, 1600, 2000],
+    // if you only output eg, 1600 (to replace just that set of files),
+    // update the fileNumber sequence used to make each filename, eg `[3,9]` > `filename-03@16_9.jpg` `filename-09@16_9.jpg`
+    fileNumber: [], // default: will use the index + 1, filenames start 01, 02, 03, ...
+};
 
-const dir_in = "./img/in/";
-const dir_out = "./img/out/";
+const avifOptions = {
+    quality: [50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100], // default: 50
+    // 0: slow & small, 9: fast & large
+    // in testing 6,7,8,9 produce identical results to each other
+    speed: [0, 1, 2, 3, 4, 5, 9], // default: 5
+    // '4:2:2' half of the color info of 4:4:4 // but not supported by sharp
+    // '4:2:0' quarter of the color info of 4:4:4
+    chromaSubsampling: ['4:4:4', '4:2:0'], // default: '4:4:4'
+};
 
-// const g_pattern = `*.jpg,*.jpeg,*.png`;
-const g_pattern = `*.jpg`;
-const g_options = {cwd: dir_in, stat: true, nodir: true, mark: true};
+const heifOptions = {
+    quality: [50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100], // default: 50
+    compression: ['av1'], // default: 'av1' // doesn't like 'hevc'
+    // 0: slow & small, 9: fast & large
+    speed: [0, 1, 2, 3, 4, 5, 9], // default: 5
+    // '4:2:2' half of the color info of 4:4:4 // but not supported by sharp
+    // '4:2:0' quarter of the color info of 4:4:4
+    chromaSubsampling: ['4:4:4', '4:2:0'], // default: '4:4:4'
+};
 
-const filesarray = glob.sync(g_pattern, g_options);
+// Variable Image Quality depending on size
+config.qualities = {
+    jpg: {
+        lg: {
+            size: 1600,
+            quality: 60,
+        },
+        md: {
+            size: 1000,
+            quality: 65,
+        },
+        sm: {
+            size: 0,
+            quality: 70,
+        },
+    },
+    webp: {
+        lg: {
+            size: 1600,
+            quality: 55,
+        },
+        md: {
+            size: 1000,
+            quality: 60,
+        },
+        sm: {
+            size: 0,
+            quality: 65,
+        },
+    },
+};
 
-// List files that are involved
-console.table(filesarray);
+const imageQualities = (size, type) => {
+    if (size > config.qualities[type].lg.size) {
+        return config.qualities[type].lg.quality;
+    }
+    if (size > config.qualities[type].md.size) {
+        return config.qualities[type].md.quality;
+    }
+    return config.qualities[type].sm.quality;
+};
 
-// 2000px wide, jpg @ 60, webp @ 55
-// 1600px wide, jpg @ 65, webp @ 60
-// 1000px wide, jpg @ 70, webp @ 65
+const globOptions = {
+    cwd: config.input,
+    stat: true,
+    nodir: true,
+    mark: true,
+};
+const filesArray = glob.sync(config.globPattern, globOptions);
+
+// Work out total number of images
+let totalImages = 0;
+totalImages += config.makeJpeg ? 1 : 0;
+totalImages += config.makeWebp ? 1 : 0;
+totalImages += config.makeAvif ? 1 : 0;
+totalImages += config.makeHeif ? 1 : 0;
+
+const totalRegularImages = config.makeRectangles ? config.imageSizes.length : 0;
+const totalRatioImages = config.makeRatioCrops
+    ? config.positionStrategies.length * config.imageRatios.length * config.imageSizes.length
+    : 0;
+
+// List actual files that are involved
+console.table(filesArray);
+// List the config
+console.table({
+    'CPU threads': sharp.concurrency(),
+    imageRatios: config.imageRatios.length,
+    positionStrategies: config.positionStrategies.length,
+    imageSizes: config.imageSizes.length,
+    'Total image variants': totalImages * (totalRegularImages + totalRatioImages),
+    'Grand total images to be made': filesArray.length * (totalImages * (totalRegularImages + totalRatioImages)),
+});
+
+const promises = [];
+const sharpStream = sharp({
+    failOnError: false,
+});
+
+const imageStreamJpeg = function (
+    imageSize,
+    basename,
+    imageNumber,
+    resizeOptions = {
+        width: imageSize,
+    },
+    filename = `${config.output}${basename}-${imageNumber}`
+) {
+    promises.push(
+        sharpStream
+            .setMaxListeners(0)
+            .clone()
+            .resize(resizeOptions)
+            .jpeg({ quality: imageQualities(imageSize, 'jpg') })
+            .toFile(`${filename}.jpg`)
+    );
+};
+
+const imageStreamWebp = function (
+    imageSize,
+    basename,
+    imageNumber,
+    resizeOptions = {
+        width: imageSize,
+    },
+    filename = `${config.output}${basename}-${imageNumber}`
+) {
+    promises.push(
+        sharpStream
+            .setMaxListeners(0)
+            .clone()
+            .resize(resizeOptions)
+            .webp({ quality: imageQualities(imageSize, 'webp') })
+            .toFile(`${filename}.webp`)
+    );
+};
+
+const imageStreamAvif = function (
+    imageSize,
+    basename,
+    imageNumber,
+    resizeOptions = {
+        width: imageSize,
+    },
+    filename = `${config.output}${basename}-${imageNumber}`
+) {
+    avifOptions.quality.forEach((qly) => {
+        avifOptions.speed.forEach((spd) => {
+            avifOptions.chromaSubsampling.forEach((cs) => {
+                const csname = cs.replace(/:/g, '');
+                promises.push(
+                    sharpStream
+                        .setMaxListeners(0)
+                        .clone()
+                        .resize(resizeOptions)
+                        .avif({
+                            quality: qly,
+                            lossless: false,
+                            speed: spd,
+                            chromaSubsampling: cs,
+                        })
+                        .toFile(`${filename}-${qly}-${spd}-${csname}.avif`)
+                );
+            });
+        });
+    });
+};
+
+const imageStreamHeif = function (
+    imageSize,
+    basename,
+    imageNumber,
+    resizeOptions = {
+        width: imageSize,
+    },
+    filename = `${config.output}${basename}-${imageNumber}`
+) {
+    heifOptions.quality.forEach((qly) => {
+        heifOptions.speed.forEach((spd) => {
+            heifOptions.compression.forEach((comp) => {
+                heifOptions.chromaSubsampling.forEach((cs) => {
+                    const csname = cs.replace(/:/g, '');
+                    promises.push(
+                        sharpStream
+                            .setMaxListeners(0)
+                            .clone()
+                            .resize(resizeOptions)
+                            .heif({
+                                quality: qly,
+                                lossless: false,
+                                compression: comp,
+                                speed: spd,
+                                chromaSubsampling: cs,
+                            })
+                            .toFile(`${filename}-${qly}-${spd}-${comp}-${csname}.heif`)
+                    );
+                });
+            });
+        });
+    });
+};
+
+const makeImages = function (options) {
+    const { imageSize, basename, imageNumber, resizeOptions, filename } = options;
+
+    if (config.makeJpeg) {
+        imageStreamJpeg(imageSize, basename, imageNumber, resizeOptions, filename);
+    }
+    if (config.makeWebp) {
+        imageStreamWebp(imageSize, basename, imageNumber, resizeOptions, filename);
+    }
+    if (config.makeAvif) {
+        imageStreamAvif(imageSize, basename, imageNumber, resizeOptions, filename);
+    }
+    if (config.makeHeif) {
+        imageStreamHeif(imageSize, basename, imageNumber, resizeOptions, filename);
+    }
+};
 
 function sharprun(basename, file) {
+    config.imageSizes.forEach((imageSize, i) => {
+        // In case the amount of filename numbers in the array dont match the amount of sizes
+        const imageFileNumber = config.fileNumber.length === config.imageSizes.length ? config.fileNumber[i] : i + 1;
+        const imageNumber = imageFileNumber.toString().padStart(2, '0');
 
-    const sharpStream = sharp({
-        failOnError: false
-    });
+        // ORIGINAL RECTAGULAR CROPS
+        if (config.makeRectangles) {
+            makeImages({ imageSize, basename, imageNumber });
+        }
 
-    const promises = [];
+        // RATIO CROPS
+        if (config.makeRatioCrops) {
+            config.positionStrategies.forEach(([strategyLabel, strategyType]) => {
+                let strategyPosition;
 
-    // Crop widths that will be output
-    // const i_sizes = [800];
-    const i_sizes = [100,150,200,400,600,800,1000,1200,1600,2000];
-
-    // Crop ratios that will be output
-    const ratios = [[4,6], [16,9], [22,9], [1,1]];
-
-    const generateRectangles = true;
-    const generateRatioCrops = true;
-    const generateJpeg = true;
-    const generateWebp = true;
-
-    // Smarter cropping - 'Attention' is better in majority, but not always
-    // Alternative is to use much-faster position: 'centre'
-    const positionStrategy = ["centre", "attention", "entropy"];
-    // const positionStrategy = ["centre"];
-    // const positionStrategy = ["attention"];
-
-    // Used for looping the ratios and formats
-    let i = 1;
-    let j = 1;
-
-    // RECTANGULAR
-    if (generateRectangles) {
-        i_sizes.forEach(img_size => {
-            const jpeg_quality = (img_size > 1600) ? 60 : (img_size > 1000) ? 65 : 70;
-            const webp_quality = (img_size > 1600) ? 55 : (img_size > 1000) ? 60 : 65;
-            // console.info(`Outputting rectangle size: ${i}`);
-
-            // sample-15-07.jpg >> {sample-15}-{07}.jpg {filename}-{img_num}.ext
-            // sample-15-07@22_9.jpg >> {sample-15}-{07}@{22_9}.jpg {filename}-{img_num}@{img_ratio}.ext
-            let img_num = i.toString().padStart(2, '0');
-
-            // JPEG output
-            if (generateJpeg) {
-                promises.push(
-                    sharpStream
-                        .setMaxListeners(0)
-                        .clone()
-                        .resize({ width: img_size })
-                        .jpeg({quality: jpeg_quality})
-                        .toFile(`${dir_out}${basename}-${img_num}.jpg`)
-                );
-            };
-
-            // WEBP output
-            if (generateWebp) {
-                promises.push(
-                    sharpStream
-                        .setMaxListeners(0)
-                        .clone()
-                        .resize({ width: img_size })
-                        .webp({quality: webp_quality})
-                        .toFile(`${dir_out}${basename}-${img_num}.webp`)
-                );
-            };
-
-            i++;
-        });
-    }
-
-    // RATIO CROPS
-    if (generateRatioCrops) {
-        i_sizes.forEach(img_size => {
-            const jpeg_quality = (img_size > 1600) ? 60 : (img_size > 1000) ? 65 : 70;
-            const webp_quality = (img_size > 1600) ? 55 : (img_size > 1000) ? 60 : 65;
-            // console.info(`Outputting cropped size: ${j}`);
-
-            // sample-15-07.jpg >> {sample-15}-{07}.jpg {filename}-{img_num}.ext
-            // sample-15-07@22_9.jpg >> {sample-15}-{07}@{22_9}.jpg {filename}-{img_num}@{img_ratio}.ext
-            let img_num = j.toString().padStart(2, '0');
-
-            positionStrategy.forEach(img_strategy => {
-                let strategyLabel;
-                let this_strategy;
-
-                // console.log(`img_strategy: ${img_strategy}`);
-
-                if (img_strategy === "centre"){
-                    strategyLabel = "-" + img_strategy.charAt(0);
-                    this_strategy = img_strategy;
-                }
-                else if (img_strategy === "attention"){
-                    strategyLabel = "";
-                    this_strategy = sharp.strategy.attention;
-                }
-                else {
-                    strategyLabel = "-" + img_strategy.charAt(0);
-                    this_strategy = sharp.strategy.entropy;
+                if (strategyType === 'centre') {
+                    strategyPosition = strategyType;
+                } else if (strategyType !== '') {
+                    strategyPosition = sharp.strategy[strategyType];
+                } else {
+                    strategyPosition = sharp.strategy.entropy;
                 }
 
-                ratios.forEach(ratio => {
+                config.imageRatios.forEach(([ratioHeight, ratioWidth]) => {
+                    const imageRatio = ratioWidth / ratioHeight;
+                    const resizeOptions = {
+                        width: imageSize,
+                        height: Math.round(imageSize * imageRatio),
+                        fit: sharp.fit.cover,
+                        position: strategyPosition,
+                    };
+                    const filename = `${config.output}${basename}-${imageNumber}@${ratioHeight}_${ratioWidth}${strategyLabel}`;
 
-                    const r1 = ratio[0];
-                    const r2 = ratio[1];
-                    const img_ratio = (r2 / r1);
-
-                    // JPEG output
-                    if (generateJpeg) {
-                        promises.push(
-                            sharpStream
-                                .setMaxListeners(0)
-                                .clone()
-                                .resize({
-                                    width: img_size,
-                                    height: Math.round(img_size * img_ratio),
-                                    fit: sharp.fit.cover,
-                                    position: this_strategy
-                                    // position: sharp.strategy.entropy
-                                })
-                                .jpeg({quality: jpeg_quality})
-                                .toFile(`${dir_out}${basename}-${img_num}@${r1}_${r2}${strategyLabel}.jpg`)
-                        );
-                    }
-
-                    // WEBP output
-                    if (generateWebp) {
-                        promises.push(
-                            sharpStream
-                                .setMaxListeners(0)
-                                .clone()
-                                .resize({
-                                    width: img_size,
-                                    height: Math.round(img_size * img_ratio),
-                                    fit: sharp.fit.cover,
-                                    position: this_strategy
-                                    // position: sharp.strategy.entropy
-                                })
-                                .webp({quality: webp_quality})
-                                .toFile(`${dir_out}${basename}-${img_num}@${r1}_${r2}${strategyLabel}.webp`)
-                        );
-                    }
+                    makeImages({ imageSize, basename, imageNumber, resizeOptions, filename });
                 });
-
             });
-            j++;
-        });
-    }
-
-    // const i_qualities = [100,70,65,60,55,50,45,40,35,30,5,1];
-    // i_qualities.forEach(img_q => {
-    //     promises.push(
-    //         sharpStream
-    //             .setMaxListeners(0)
-    //             .clone()
-    //             .resize({ width: img_size })
-    //             .webp({
-    //                 quality: img_q
-    //             })
-    //             .toFile(`${dir_out}${basename}@${img_q}.webp`)
-    //     );
-    // });
+        }
+    });
 
     // If all the same size, resize it here, then pipe it to the loop
-    // sharp(`${dir_in}${file}`).resize({ width: 400 }).pipe(sharpStream);
-    sharp(`${dir_in}${file}`).pipe(sharpStream);
+    // sharp(`${config.input}${file}`).resize({ width: 400 }).pipe(sharpStream);
+    sharp(`${config.input}${file}`).pipe(sharpStream);
 
     Promise.all(promises)
-    // .then(res => { console.log("Done!", res); })
-    .then(res => { console.log(sharp.counters()); }) //console.log(sharp.cache());
-    .catch(err => {
-        console.error("Error processing files, let's clean it up", err);
-        try {
-            // fs.unlinkSync("originalFile.jpg");
-            // fs.unlinkSync("optimized-500.jpg");
-            // fs.unlinkSync("optimized-500.webp");
-        } catch (e) { }
-    });
+        .then((res) => {
+            console.log(sharp.counters());
+            // console.log('Done!', res);
+            // console.log(sharp.cache());
+        })
+        .catch((err) => {
+            console.error("Error processing files, let's clean it up", err);
+            // try {
+            //     // fs.unlinkSync("originalFile.jpg");
+            //     // fs.unlinkSync("optimized-500.jpg");
+            //     // fs.unlinkSync("optimized-500.webp");
+            // } catch (e) {
+            //     console.log(e);
+            // }
+        });
 }
 
 function theImages() {
-    filesarray.forEach(file => {
+    filesArray.forEach((file) => {
         const basename = path.basename(file, path.extname(file));
         sharprun(basename, file);
     });
 }
 
 function sharpify() {
-
     // Wait for folders to be made
-    // https://github.com/lovell/sharp/issues/1052#issuecomment-349401421
+    // [Nice solution from](https://github.com/lovell/sharp/issues/1052#issuecomment-349401421)
     (async () => {
-        await mkdirp(`${dir_out}`);
-        await mkdirp(`${dir_out}`);
-      
+        await mkdirp(`${config.output}`);
+
         theImages();
-      })();
+    })();
 }
 
 sharpify();
